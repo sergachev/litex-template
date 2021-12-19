c53296c8c49b42848d74d21bcf369291ac0c2855
Ilia Sergachev <ilia@sergachev.ch>
2021-12-19 13:48:15 +0100

    add ps7 download tcl

---
+++ src/ps7_boot.tcl
@@ -0,0 +1,12 @@
+set project_name [lindex $argv 0]
+connect
+targets -set -filter {name =~ "ARM*#0"}
+rst
+stop
+source build/$project_name/gateware/$project_name.gen/sources_1/ip/Zynq/ps7_init.tcl
+ps7_init
+ps7_post_config
+
+# dow [lindex $argv 1]
+# con
+# fpga [lindex $argv 2]
