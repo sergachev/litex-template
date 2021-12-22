568d1831241c2dc0f25604c58cb64bf8d5d506f1
Ilia Sergachev <ilia@sergachev.ch>
2021-12-22 12:39:28 +0100

    Update ps7_boot.tcl

--- scripts/ps7_boot.tcl
+++ scripts/ps7_boot.tcl
@@ -1,9 +1,10 @@
-set project_name [lindex $argv 0]
+# example: xsct ps7_boot.tcl build/digilent_zedboard/gateware/digilent_zedboard.gen/sources_1/ip/Zynq/ps7_init.tcl build/digilent_zedboard/software/bios/bios.elf build/digilent_zedboard/gateware/digilent_zedboard.bit
+
 connect
 targets -set -filter {name =~ "ARM*#0"}
 rst
 stop
-source build/$project_name/gateware/$project_name.gen/sources_1/ip/Zynq/ps7_init.tcl
+source [lindex $argv 0]
 ps7_init
 ps7_post_config
 
